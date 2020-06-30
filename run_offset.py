8e36a7f7de298952387723a77e708b658730f0c8
garvita <garvitaa@buffalo.edu>
2020-06-30 11:23:53 -0500

    Added tags for applying JECs

--- run_offset.py
+++ run_offset.py
@@ -9,33 +9,38 @@ process = cms.Process("Ana")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.options.allowUnscheduled = cms.untracked.bool(True)
 
 readFiles = cms.untracked.vstring()
 process.source = cms.Source ("PoolSource", fileNames = readFiles)
 readFiles.extend( [
-  '/store/data/Run2017B/ZeroBias/AOD/17Nov2017-v1/20000/00D69F39-6BD3-E711-8F55-44A842CF05E6.root'
+  '/store/data/Run2018A/ZeroBias/AOD/ForValUL2018-v1/60000/FFD11034-32CB-7048-B09B-A57DA99BD130.root'
 ] );
 
 isMC = cms.bool(False)
 
 if isMC:
-  OutputName = "_MC"
+  OutputName = "_MC_UL2018"
+  eraName = "Summer19UL18_V2_SimpleL1_MC"
+  jetType_name = "AK4PFchs" # or "AK4PF"
 
-  #process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff" )
-  #from Configuration.AlCa.GlobalTag import GlobalTag
-  #process.GlobalTag = GlobalTag( process.GlobalTag, '76X_mcRun2_asymptotic_v12' )
+  process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff" )
+  from Configuration.AlCa.GlobalTag import GlobalTag
+  process.GlobalTag = GlobalTag( process.GlobalTag, '106X_mc2017_realistic_v6' )
 
 else:
-  OutputName = "_Data"
+  run = "A"
+  OutputName = "_Data_UL2018"+run
+  eraName = "Summer19UL18_Run"+run+"_V2_SimpleL1_DATA"
+  jetType_name = "AK4PFchs" # or "AK4PF"
 
   process.load( "Configuration.Geometry.GeometryIdeal_cff" )
   process.load( "Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff" )
   process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff" )
   from Configuration.AlCa.GlobalTag import GlobalTag
-  process.GlobalTag = GlobalTag( process.GlobalTag, '94X_dataRun2_ReReco17_forValidation' )
+  process.GlobalTag = GlobalTag( process.GlobalTag, '106X_dataRun2_v24' )
 
   # ZeroBias Trigger
   process.HLTZeroBias =cms.EDFilter("HLTHighLevel",
@@ -58,9 +63,9 @@ else:
   )
 
 process.pf = cms.EDAnalyzer("OffsetTreeMaker",
-    numSkip = cms.int32(101),
+    numSkip = cms.int32(1),
     RootFileName = cms.string("Offset" + OutputName + ".root"),
-    puFileName = cms.string("lumi-per-bx.root"),
+    puFileName = cms.string("pileup_2018.txt"),
     isMC = isMC,
     writeCands = cms.bool(True),
     trackTag = cms.InputTag("generalTracks"),
@@ -68,9 +73,19 @@ process.pf = cms.EDAnalyzer("OffsetTreeMaker",
     pvTag = cms.InputTag("offlinePrimaryVertices"),
     muTag = cms.InputTag("addPileupInfo"),
     rhoTag = cms.InputTag("fixedGridRhoFastjetAll"),
+    rhoCTag = cms.InputTag("fixedGridRhoFastjetAll"),
     rhoC0Tag = cms.InputTag("fixedGridRhoFastjetCentralNeutral"),
     rhoCCTag = cms.InputTag("fixedGridRhoFastjetCentralChargedPileUp"),
-    pfJetTag = cms.InputTag("ak4PFJetsCHS")
+    pfJetTag = cms.InputTag("ak4PFJetsCHS"),
+    era = cms.string(eraName),
+    jet_type = cms.string(jetType_name),
+    doL1L2L3Res = cms.bool(True)
+    #miniAOD
+    #trackTag = cms.InputTag("lostTracks"),
+    #pfTag = cms.InputTag("packedPFCandidates"),
+    #pvTag = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    #muTag = cms.InputTag("slimmedAddPileupInfo"),
+    #pfJetTag = cms.InputTag("slimmedJets")
 )
 
 process.myseq = cms.Sequence( process.pf )
